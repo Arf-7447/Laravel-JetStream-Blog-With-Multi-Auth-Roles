1dac17808f240dc672ad07ee9cdd415a384e83c0
Arf-7447 <arippandu55@gmail.com>
2024-06-05 11:41:30 +0700

    adding comment

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -4,5 +4,5 @@
         <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
     </svg>
 </a> --}}
-
+{{-- this is for logo --}}
 <img class="h-20 w-auto rounded-3xl border-4 border-teal-600 dark:border-white" src="{{ asset('images/company.png') }}" alt="Ivanov Insights">
